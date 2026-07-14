0e4bda8a54c95957b2d604b66e0be6729db5bd2a
zy <zy-inbox@outlook.com>
2026-07-14 11:08:40 +0800

    Update Wubi dictionary and candidate behavior

--- lua/date_translator.lua
+++ lua/date_translator.lua
@@ -1,9 +1,9 @@
 -- 日期时间，可在方案中配置触发关键字。
 
 -- 提高权重的原因：因为在方案中设置了大于 1 的 initial_quality，导致 rq sj xq dt ts 产出的候选项在所有词语的最后。
-local function yield_cand(seg, text)
+local function yield_cand(seg, text, quality)
     local cand = Candidate('', seg.start, seg._end, text, '')
-    cand.quality = 100
+    cand.quality = quality
     yield(cand)
 end
 
@@ -17,44 +17,45 @@ function M.init(env)
     M.week = config:get_string(env.name_space .. '/week') or 'xq'
     M.datetime = config:get_string(env.name_space .. '/datetime') or 'dt'
     M.timestamp = config:get_string(env.name_space .. '/timestamp') or 'ts'
+    M.quality = config:get_int(env.name_space .. '/quality') or 100
 end
 
 function M.func(input, seg, env)
     -- 日期
     if (input == M.date) then
         local current_time = os.time()
-        yield_cand(seg, os.date('%Y-%m-%d', current_time))
-        yield_cand(seg, os.date('%Y/%m/%d', current_time))
-        yield_cand(seg, os.date('%Y.%m.%d', current_time))
-        yield_cand(seg, os.date('%Y%m%d', current_time))
-        yield_cand(seg, os.date('%Y年%m月%d日', current_time):gsub('年0', '年'):gsub('月0','月'))
+        yield_cand(seg, os.date('%Y-%m-%d', current_time), M.quality)
+        yield_cand(seg, os.date('%Y/%m/%d', current_time), M.quality)
+        yield_cand(seg, os.date('%Y.%m.%d', current_time), M.quality)
+        yield_cand(seg, os.date('%Y%m%d', current_time), M.quality)
+        yield_cand(seg, os.date('%Y年%m月%d日', current_time):gsub('年0', '年'):gsub('月0','月'), M.quality)
 
     -- 时间
     elseif (input == M.time) then
         local current_time = os.time()
-        yield_cand(seg, os.date('%H:%M', current_time))
-        yield_cand(seg, os.date('%H:%M:%S', current_time))
+        yield_cand(seg, os.date('%H:%M', current_time), M.quality)
+        yield_cand(seg, os.date('%H:%M:%S', current_time), M.quality)
 
     -- 星期
     elseif (input == M.week) then
         local current_time = os.time()
         local week_tab = {'日', '一', '二', '三', '四', '五', '六'}
         local text = week_tab[tonumber(os.date('%w', current_time) + 1)]
-        yield_cand(seg, '星期' .. text)
-        yield_cand(seg, '礼拜' .. text)
-        yield_cand(seg, '周' .. text)
+        yield_cand(seg, '星期' .. text, M.quality)
+        yield_cand(seg, '礼拜' .. text, M.quality)
+        yield_cand(seg, '周' .. text, M.quality)
 
     -- ISO 8601/RFC 3339 的时间格式 （固定东八区）（示例 2022-01-07T20:42:51+08:00）
     elseif (input == M.datetime) then
         local current_time = os.time()
-        yield_cand(seg, os.date('%Y-%m-%dT%H:%M:%S+08:00', current_time))
-        yield_cand(seg, os.date('%Y-%m-%d %H:%M:%S', current_time))
-        yield_cand(seg, os.date('%Y%m%d%H%M%S', current_time))
+        yield_cand(seg, os.date('%Y-%m-%dT%H:%M:%S+08:00', current_time), M.quality)
+        yield_cand(seg, os.date('%Y-%m-%d %H:%M:%S', current_time), M.quality)
+        yield_cand(seg, os.date('%Y%m%d%H%M%S', current_time), M.quality)
 
     -- 时间戳（十位数，到秒，示例 1650861664）
     elseif (input == M.timestamp) then
         local current_time = os.time()
-        yield_cand(seg, string.format('%d', current_time))
+        yield_cand(seg, string.format('%d', current_time), M.quality)
     end
 
     -- -- 显示内存
